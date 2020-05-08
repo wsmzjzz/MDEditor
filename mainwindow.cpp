faffa5ab66ff1b4426123dc89bc3759624578530
Meng Zijian <1476287221@qq.com>
2020-05-08 11:33:46 +0800

    full file actions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,16 +23,55 @@ MainWindow::~MainWindow()
 }
 
 
+bool MainWindow::isModified() const
+{
+    return ui->editBox->document()->isModified();
+}
 
+void MainWindow::openFile(const QString &path)
+{
+    QFile f(path);
+    if (!f.open(QIODevice::ReadOnly)) {
+        QMessageBox::warning(this, windowTitle(),
+                             tr("无法打开文件 file %1: %2").arg(
+                                 QDir::toNativeSeparators(path), f.errorString()));
+        return;
+    }
+    mdFilePath = path;
+    ui->editBox->setPlainText(f.readAll());
+}
 
 void MainWindow::on_actionOpen_File_triggered()
 {
-    ui->editBox->setPlainText("open" + mdContent.mdCodeText);
+  if (isModified()) {
+      QMessageBox::StandardButton button = QMessageBox::question(this, windowTitle(),
+                           tr("你有未保存的改动，是否仍要打开文档？"));
+      if (button != QMessageBox::Yes)
+          return;
+  }
+
+  QString path = QFileDialog::getOpenFileName(this,
+      tr("打开Markdown文件"), "", tr("Markdown文件 (*.md)"));
+  if (path.isEmpty())
+      return;
+
+  openFile(path);
+//    ui->editBox->setPlainText("open" + mdContent.mdCodeText);
 }
 
 void MainWindow::on_actionNew_File_triggered()
 {
-    ui->editBox->setPlainText("new");
+  if (isModified()) {
+      QMessageBox::StandardButton button = QMessageBox::question(this, windowTitle(),
+                           tr("你有未保存的改动，是否仍要创建新文档？"));
+      if (button != QMessageBox::Yes)
+          return;
+  }
+
+  mdFilePath.clear();
+  ui->editBox->setPlainText(tr("## 新文档"));
+  ui->editBox->document()->setModified(false);
+//    ui->editBox->setPlainText("new");
 }
 
 void MainWindow::on_actionSave_File_triggered()
@@ -45,7 +84,7 @@ void MainWindow::on_actionSave_File_triggered()
   QFile f(mdFilePath);
   if (!f.open(QIODevice::WriteOnly | QIODevice::Text))  {
       QMessageBox::warning(this, windowTitle(),
-                           tr("Could not write to file %1: %2").arg(
+                           tr("无法写入文件 %1: %2").arg(
                                QDir::toNativeSeparators(mdFilePath), f.errorString()));
       return;
   }
@@ -53,16 +92,16 @@ void MainWindow::on_actionSave_File_triggered()
   str << ui->editBox->toPlainText();
 
   ui->editBox->document()->setModified(false);
-    ui->editBox->setPlainText("save");
+//    ui->editBox->setPlainText("save");
 }
 
 void MainWindow::on_actionSave_As_triggered()
 {
   QString path = QFileDialog::getSaveFileName(this,
-      tr("Save MarkDown File"), "", tr("MarkDown File (*.md, *.markdown)"));
+      tr("保存Markdown文件"), "", tr("Markdown文件 (*.md, *.markdown)"));
   if (path.isEmpty())
       return;
   mdFilePath = path;
   on_actionSave_File_triggered();
-    ui->editBox->setPlainText("save as");
+//    ui->editBox->setPlainText("save as");
 }
